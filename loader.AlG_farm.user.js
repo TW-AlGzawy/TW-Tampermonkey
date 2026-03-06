5a471ba8732dcd82fe466dbc7fd0998819045dbc
TW-AlGzawy <karm.saleh.usa2@gmail.com>
2026-03-06 03:45:38 +0100

    Update loader.AlG_farm.user.js

--- loader.AlG_farm.user.js
+++ loader.AlG_farm.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         AlGzawy - Farm Bot Loader
+// @name         AlGzawy - Farm Bot professional 1.1
 // @namespace    AlGzawy-Scripts-farm-loader
 // @version      1.1
 // @description  يقوم بتحميل وتشغيل بوت النهب الآمن من AlGzawy
