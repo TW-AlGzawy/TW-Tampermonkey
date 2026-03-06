8809cb06b27a24102ec84ba19dc5d768421f3dc5
TW-AlGzawy <karm.saleh.usa2@gmail.com>
2026-03-06 01:53:09 +0100

    Update loader.AlG_farm.js

--- loader.AlG_farm.js
+++ loader.AlG_farm.js
@@ -1,1 +1,40 @@
+// ==UserScript==
+// @name        Farm_bot_algzawy
+// @namespace    AlGzawy-Scripts-farm
+// @version      1.1
+// @description  سكربت يقوم بالنهب بمساعد النهب بشكل تلقائي
+// @author       AlGzawy
+// @match        https://*.tribalwars.ae/game.php?*
+// @connect      raw.githubusercontent.com
+// @icon  https://files.manuscdn.com/user_upload_by_module/session_file/310419663029215752/GYTOxdyXXZqmFprq.jpg
+// @grant        GM_xmlhttpRequest
+// @run-at       document-end
+// ==/UserScript==
 
+(function( ) {
+    'use strict';
+
+   
+    const scriptUrl = 'https://raw.githubusercontent.com/TW-AlGzawy/TW-Tampermonkey/main/AlG_farm.js';
+
+    console.log('جاري تحميل سكربت النهب غزااوي...' );
+
+    GM_xmlhttpRequest({
+        method: "GET",
+        url: scriptUrl,
+        onload: function(response ) {
+            if (response.status === 200) {
+                console.log('تم التحميل بنجاح، جاري التشغيل...');
+                
+                new Function(response.responseText)();
+            } else {
+                console.error('فشل تحميل السكربت. الحالة:', response.status);
+                alert('فشل تحميل سكربت النهب. يرجى التواصل مع المطور.');
+            }
+        },
+        onerror: function(response) {
+            console.error('حدث خطأ في الشبكة أثناء تحميل السكربت.');
+            alert('حدث خطأ في الشبكة. تأكد من اتصالك بالإنترنت أو أن @connect يعمل بشكل صحيح.');
+        }
+    });
+})();
