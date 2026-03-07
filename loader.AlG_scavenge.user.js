4a2397697b4b2d0b9648d12181ed0cde62c1eb47
TW-AlGzawy <karm.saleh.usa2@gmail.com>
2026-03-07 06:14:10 +0100

    Update loader.AlG_scavenge.user.js

--- loader.AlG_scavenge.user.js
+++ loader.AlG_scavenge.user.js
@@ -1,19 +1,42 @@
 // ==UserScript==
-// @name         loader.AlG_scavenge.user.js
-// @namespace    http://tampermonkey.net/
-// @version      10.0
-// @description  يقوم بتحميل وتشغيل بوت الإغارات المدمج الخاص بـ AlGzawy
+// @name         AlG_scavenge- الأغارات [v1.4]
+// @namespace    AlGzawy-Scripts-scavenge
+// @version      1.4
+// @description  سكربت يقوم بتشغيل سكربت الاغارات المشهور تلقائيا 
 // @author       AlGzawy
 // @match        https://*/*=scavenge_mass*
-// @grant        none
-// @require      https://code.jquery.com/jquery-3.6.0.min.js
-// @require      https://code.jquery.com/ui/1.12.1/jquery-ui.min.js
-// @require      https://github.com/TW-AlGzawy/TW-Tampermonkey/raw/main/AlG_scavenge.js
+// @updateURL    https://raw.githubusercontent.com/TW-AlGzawy/TW-Tampermonkey/main/loader.AlG_scavenge.user.js
+// @downloadURL  https://raw.githubusercontent.com/TW-AlGzawy/TW-Tampermonkey/main/loader.AlG_scavenge.user.js
+// @icon         https://files.manuscdn.com/user_upload_by_module/session_file/310419663029215752/GYTOxdyXXZqmFprq.jpg
+// @connect      raw.githubusercontent.com
+// @grant        GM_xmlhttpRequest
+// @run-at       document-end
 // ==/UserScript==
 
 (function( ) {
     'use strict';
-    // Tampermonkey has already loaded and executed the script from the @require URL.
-    // No additional code is needed here.
-    console.log('[AlGzawy Loader] تم تحميل وتنفيذ البوت المدمج من GitHub بنجاح.');
+
+   
+    const scriptUrl = 'https://raw.githubusercontent.com/TW-AlGzawy/TW-Tampermonkey/main/AlG_scavenge.js';
+
+    console.log('جاري تحميل سكربت اغارات غزااوي...' );
+
+    GM_xmlhttpRequest({
+        method: "GET",
+        url: scriptUrl,
+        onload: function(response ) {
+            if (response.status === 200) {
+                console.log('تم التحميل بنجاح، جاري التشغيل...');
+                
+                new Function(response.responseText)();
+            } else {
+                console.error('فشل تحميل السكربت. الحالة:', response.status);
+                alert('فشل تحميل سكربت الإغارات. يرجى التواصل مع المطور.');
+            }
+        },
+        onerror: function(response) {
+            console.error('حدث خطأ في الشبكة أثناء تحميل السكربت.');
+            alert('حدث خطأ في الشبكة. تأكد من اتصالك بالإنترنت أو أن @connect يعمل بشكل صحيح.');
+        }
+    });
 })();
