62dbdd8a11bc3b87ce49265bb25ae94cb7d42c23
AlGzawy <karm.saleh.usa2@gmail.com>
2026-03-19 01:43:22 +0100

    feat: loader for AlG attacks Telegram alert

--- loader.AlG_attacks.user.js
+++ loader.AlG_attacks.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AlGzawy - تنبيه الهجمات (Telegram)
 // @namespace    AlGzawy-Scripts-attacks-loader
-// @version      1.7
+// @version      1.8
 // @description  ينبهك على التليجرام عند وجود هجمات على قريتك في Tribal Wars
 // @author       AlGzawy
 // @include      https://*.tribalwars.*/game.php*
